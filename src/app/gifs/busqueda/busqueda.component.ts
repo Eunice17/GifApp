0e021edcbce9fee32967ce77c1a5a878ddb58930
unknown <euniceancajima@gmail.com>
2022-02-28 00:57:56 -0500

    Consumiendo servicio de historial de búsqueda

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,13 +10,13 @@ export class BusquedaComponent implements OnInit {
 
   @ViewChild('inputBuscar') inputBuscar!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifService: GifsService) { }
 
   ngOnInit(): void {  }
 
   buscar() {
     let value = this.inputBuscar?.nativeElement.value;
-    console.log("captura",value);
+    this.gifService.setHistorial(value);
     this.inputBuscar!.nativeElement.value = '';
   }
 
